612250dac6d95b6e03ab92dc41d2d45ea4b243c8
Tyler Johnson <tylerjohnson139@gmail.com>
2019-08-09 21:56:47 -0700

    update file location and html / js files

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,4 @@
+var A = "The RP said \"I'm so tired of them parking here\\there\"" + " I told them \"Stop being so petty\"", B = "Then I lost my job.", C = (5+5)
+var B = B.fontcolor("red")
+
+document.write(C);
